99741a3724982566de540c54c483f92f235d5d3b
Michael Bowen <mikee.bowen@gmail.com>
2019-08-16 19:28:06 -0700

    get correct number of lines and syllables, asynchronous call still causes order issues

--- index.js
+++ index.js
@@ -6,73 +6,59 @@ const axios = require('axios');
 const apiHost = process.env.WORDS_API_HOST;
 const apiKey = process.env.WORDS_API_KEY;
 
-// axios.get(`https://${apiHost}/words/car/typeOf`, {
-//   headers: {
-//     'x-rapidapi-host': apiHost,
-//     'x-rapidapi-key': apiKey,
-//   },
-// })
-// .then(res => {
-//   console.log("TCL: res", res);
-// })
-// .catch(err => {
-//   console.log("TCL: err", err);
-// });
-
-function getHaikuWord() {
-  return axios.get(`https://${apiHost}/words/hatchback`, {
-    headers: {
-      'x-rapidapi-host': apiHost,
-      'x-rapidapi-key': apiKey,
-    },
-  });
-
-}
-
-
-async function getHaiku() {
-  const haiku = [];
-  const haikuSyllables = [5, 7, 5];
+async function getHaiku(syllables) {
+  let haiku;
   let syllableCount =  0;
+  let callCount = 0;
+  while(syllableCount < syllables) {
+    callCount++;
+    const data = await axios.get(`https://${apiHost}/words/`, {
+      headers: {
+        'x-rapidapi-host': apiHost,
+        'x-rapidapi-key': apiKey,
+      },
+      params: {
+        random: true,
+        soundsMax: 12,
+        limit: 100,
+      },
+    })
+    .catch(e => {
+      console.error("TCL: e", e)
+    });
 
-  haikuSyllables.forEach(async(hs, i) => {
-    while(syllableCount < hs) {
-      const {data: {word}} = await axios.get(`https://${apiHost}/words/`, {
-        headers: {
-          'x-rapidapi-host': apiHost,
-          'x-rapidapi-key': apiKey,
-        },
-        params: {
-          random: true,
-          soundsMax: 12,
-        },
-      })
-      .catch(e => {
-        console.error("TCL: e", e)
-      });
-      console.log("TCL: getHaiku -> obj", word);
+  const word = data.data.word;
+  const syllablesInWord = countSyllables(word);
 
-      const syllablesInWord = countSyllables(word);
-
-      if (syllableCount + syllablesInWord <= hs) {
-        if (haiku[i]) {
-          haiku[i] += ` ${word}`;
-        } else {
-          haiku[i] = word;
-        }
-
-        syllableCount += syllablesInWord;
+    if (syllableCount + syllablesInWord <= syllables) {
+      if (haiku) {
+        haiku += ` ${word}`;
+      } else {
+        haiku = word;
       }
-    }
-    haiku.forEach(h => console.log(h));
-  });
 
+      syllableCount += syllablesInWord;
+    }
+  }
+  console.log(callCount);
+  console.log(haiku);
 }
 
-// getHaiku();
+for (let i = 0; i < 3; i++) {
+  let syllables;
 
-getHaiku()
-.catch(e => {
-console.log("TCL: e", e)
-
-})
+  switch (i) {
+    case 0:
+      syllables = 5;
+      break;
+    case 1:
+      syllables = 7;
+      break;
+    case 2:
+      syllables = 5;
+      break;
+    default:
+      break;
+  }
+  getHaiku(syllables);
+}
