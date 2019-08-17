9bb9d7b5a1ee5b91d98c570a5482e09d21db26f0
Michael Bowen <mikee.bowen@gmail.com>
2019-08-17 13:17:16 -0700

    remove syllable package and somewhate get order correct

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 require('dotenv').config();
-const lib = require('./lib');
+const {getRandomHaiku} = require('./lib');
 const apiHost = process.env.WORDS_API_HOST;
 const apiKey = process.env.WORDS_API_KEY;
-const {getRandomHaiku} = lib;
 
 getRandomHaiku(apiKey, apiHost)
 .catch(err => console.error(err));
