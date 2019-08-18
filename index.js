c10f02f2bbe37ef0f73422011ffa12df5e30b88f
Michael Bowen <mikee.bowen@gmail.com>
2019-08-17 18:12:49 -0700

    refactor getWord into its own file and add start of getSonnet function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 require('dotenv').config();
-const {getRandomHaiku} = require('./lib');
+const {getRandomHaiku, getSonnet} = require('./lib');
 const apiHost = process.env.WORDS_API_HOST;
 const apiKey = process.env.WORDS_API_KEY;
 
 getRandomHaiku(apiKey, apiHost)
 .catch(err => console.error(err));
+// getSonnet(apiKey, apiHost)
+// .catch(err => console.error(err));
